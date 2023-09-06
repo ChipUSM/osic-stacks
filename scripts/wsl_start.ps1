0433b2acea536191388438f6cc0581424d7ccaa4
Mario1159 <mario@1159.cl>
2023-09-06 01:04:33 -0300

    Add remote option

--- scripts/wsl_start.ps1
+++ scripts/wsl_start.ps1
@@ -1,14 +1,22 @@
+param($remote)
+
 $imagename = Read-Host -Prompt 'Container image to initialize'
 $containername = Read-Host -Prompt 'Container instance name [default=$imagename]'
 if (!$containername) { $containername = $imagename }
 
+if($remote -eq "remote") {
+    $image = "git.1159.cl/mario1159/$imagename"
+} else {
+    $image = $imagename
+}
+
 $command = ("docker run -d " +
-    "--name "+ $containername + " " +
+    "--name $containername " +
     "-v /tmp/.X11-unix:/tmp/.X11-unix " +
     "-v /mnt/wslg:/mnt/wsl " +
     "-e WAYLAND_DISPLAY=`$WAYLAND_DISPLAY " +
     "-e DISPLAY=`$DISPLAY " +
     "-e XDG_RUNTIME_DIR=/mnt/wslg " +
-    $imagename)
+    $image)
 
 wsl bash -ic $command
