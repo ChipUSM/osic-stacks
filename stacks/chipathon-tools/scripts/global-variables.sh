13461c39c6e37b2a4029edc18eb30d9770b8d209
Aquiles Viza <aquiles.viza@sansano.usm.cl>
2024-01-23 18:44:19 -0300

    Updated volare commit

--- stacks/chipathon-tools/scripts/global-variables.sh
+++ stacks/chipathon-tools/scripts/global-variables.sh
@@ -1,5 +1,6 @@
 export PDK_ROOT=/home/designer/.volare
-export OPEN_PDKS_COMMIT="cd1748bb197f9b7af62a54507de6624e30363943"
+#export OPEN_PDKS_COMMIT="cd1748bb197f9b7af62a54507de6624e30363943"
+export OPEN_PDKS_COMMIT="bdc9412b3e468c102d01b7cf6337be06ec6e9c9a"
 
 # This files can be downloaded directly
 # - sky130A_mr.drc
